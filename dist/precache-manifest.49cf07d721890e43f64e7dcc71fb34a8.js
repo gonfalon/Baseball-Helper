4a6c2760b30e6132682fd2ba98453e4b0d96109c
Ben <dunphy1996@gmail.com>
2021-03-03 22:30:18 -0500

    Actually Adding Dist folder

---
+++ dist/precache-manifest.49cf07d721890e43f64e7dcc71fb34a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c110f74f89ceac08fdd6",
+    "url": "/Baseball-Helper/css/app.d44d774f.css"
+  },
+  {
+    "revision": "b41df610498235c299a5",
+    "url": "/Baseball-Helper/css/chunk-vendors.18cc9222.css"
+  },
+  {
+    "revision": "eed5035d3c8a0c68a79d518fa6dd669c",
+    "url": "/Baseball-Helper/index.html"
+  },
+  {
+    "revision": "c110f74f89ceac08fdd6",
+    "url": "/Baseball-Helper/js/app.cedfeba3.js"
+  },
+  {
+    "revision": "b41df610498235c299a5",
+    "url": "/Baseball-Helper/js/chunk-vendors.1e744af5.js"
+  },
+  {
+    "revision": "dec6f885441129bf53eb1d4a9e417fb8",
+    "url": "/Baseball-Helper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Baseball-Helper/robots.txt"
+  }
+]);
